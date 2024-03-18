bb11baf7936a79aaf91c1aa865942579f7ce3b37
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-03-18 10:57:51 +0100

    dynamic shuffled array and button

--- js/script.js
+++ js/script.js
@@ -8,5 +8,34 @@ const quotes = [
     "Il/Elle est magnifique/magique",
 ];
 
+// console.log(quotes);
+
 const bingoBoard = document.querySelector('.bingo-board');
 
+const generateButton = document.getElementById('generateBoard');
+
+generateButton.addEventListener('click', generateNewBoard);
+
+function generateNewBoard() {
+    // Efface la grille précédente
+    bingoBoard.innerHTML = '';
+
+    // Générer une grille aléatoire
+    const shuffledQuotes = shuffleArray(quotes.slice());
+
+    // Générer une grille de bingo 5x5 avec des phrases aléatoires
+    for (let i = 0; i < 24; i++) {
+        const cell = document.createElement('div');
+        cell.textContent = shuffledQuotes[i];
+        bingoBoard.appendChild(cell);
+    }
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        // Échanger array[i] et array[j]
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
