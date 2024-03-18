54395f9df1982478e298485c64f21112c596b483
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-03-18 10:13:05 +0100

    array of quotes

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const quotes = [
+    "Quel bonheur d'être avec vous aujourd'hui",
+    "Il va se passer plein de choses dans cette émission",
+    "On vous a préparé une énorme surprise",
+    "Je l'adore",
+    "Ah ça c'est exceptionnel",
+    "Vous allez voir, c'est magique",
+    "Il/Elle est magnifique/magique",
+]
